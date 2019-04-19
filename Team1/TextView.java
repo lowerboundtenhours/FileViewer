40b1df9e5e4c5b6d0c6fb9233af4a69bd8748043
劉哲宇 <j2325138@gmail.com>
2019-04-19 18:17:07 +0800

    implement TextView.display()

--- Team1/TextView.java
+++ Team1/TextView.java
@@ -1,13 +1,16 @@
 public class TextView extends FileView{
-
     private String text;
-    @Override
-    public void display() {
-        throw new UnsupportedOperationException();
-    }
-
     public TextView(String text) {
         this.text = text;
     }
-}
 
+    @Override
+    public void display() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.text);
+        for (Element e: elements) {
+            sb.append(" ").append(e.representation());
+        }
+        System.out.println(sb.toString());
+    }
+}
