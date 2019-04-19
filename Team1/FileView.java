8eaa6b384b295ccdd616581b11b9c5da86f3383a
劉哲宇 <j2325138@gmail.com>
2019-04-19 17:42:43 +0800

    Unit commit

---
+++ Team1/FileView.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public abstract class FileView {
+    protected ArrayList<Element> elements;
+    public abstract void display();
+}
+
---
+++ Team1/Main.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private static Map<String, FileView> fileViews = new HashMap<>();
+    public static void read_file(String filename) {
+        List<String> allLines = new ArrayList<String>();
+        try {
+            allLines = Files.readAllLines(Paths.get(filename));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String line : allLines) {
+            String[] words = line.split(" ");
+            String textViewName = words[0];
+            String command = words[1];
+            FileView fileView = fileViews.get(textViewName);
+            if (command.equals("add")) {
+                String element = words[2];
+                fileView.addElement(element);
+            }
+            else if (command.equals("display")) {
+                fileViews.display();
+            } else {
+                if (fileViews != null) {
+                    throw new UnsupportedOperationException("Not implemented yet");
+                }
+                String text = command;
+            }
+
+
+        }
+    }
+    public static void main(String[] args) {
+        String filename = args[0];
+        read_file(filename);
+    }
+}
